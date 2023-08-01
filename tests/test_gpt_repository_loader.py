b8d56b67c61a05142b3fc87aa50f8349332a99c8
cachho <admin@ch-webdev.com>
2023-08-01 09:00:00 +0200

    chore: rename repo

--- tests/test_gpt_repository_loader.py
+++ tests/test_gpt_repository_loader.py
@@ -3,15 +3,13 @@ import shutil
 import tempfile
 import unittest
 
-from gpt_repository_loader.gpt_repository_loader import (
-    get_ignore_list,
+from repo_loader.repo_loader import (
+    build_ignore_list,
     process_repository,
 )
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-GPT_IGNORE_PATH = os.path.join(
-    PROJECT_ROOT, "src", "gpt_repository_loader", ".gptignore"
-)
+GPT_IGNORE_PATH = os.path.join(PROJECT_ROOT, "src", "repo_loader", ".gptignore")
 
 
 class TestGPTRepositoryLoader(unittest.TestCase):
@@ -22,35 +20,35 @@ class TestGPTRepositoryLoader(unittest.TestCase):
     def test_end_to_end(self):
         # Set up the output file and the expected output file paths
         output_file_path = os.path.join(tempfile.mkdtemp(), "output.txt")
-        expected_output_file_path = os.path.join(
-            self.test_data_path, "expected_output.txt"
-        )
+        expected_output_file_path = os.path.join(self.test_data_path, "expected_output.txt")
 
         # Create an ignore list for the example repository
         ignore_file_path = os.path.join(self.example_repo_path, ".gptignore")
         if os.path.exists(ignore_file_path):
-            ignore_list = get_ignore_list(ignore_file_path)
+            ignore_list = build_ignore_list(self.example_repo_path, ".gptignore")
         else:
             ignore_list = []
 
         # Run the gpt-repository-loader script on the example repository
         with open(output_file_path, "w") as output_file:
-            process_repository(self.example_repo_path, ignore_list, output_file)
+            process_repository(
+                self.example_repo_path,
+                ignore_list,
+                output_file,
+                use_progress_bar=False,
+                is_quiet=True,
+            )
 
         # Compare the output to the expected output
 
         lines_expected = open(  # pylint: disable=consider-using-with
             expected_output_file_path
         ).readlines()
-        lines_actual = open(
-            output_file_path
-        ).readlines()  # pylint: disable=consider-using-with
+        lines_actual = open(output_file_path).readlines()  # pylint: disable=consider-using-with
 
         self.assertEqual(len(lines_expected), len(lines_actual))
         for i in range(len(lines_expected)):  # pylint: disable=consider-using-enumerate
-            self.assertEqual(
-                lines_expected[i].replace("/", os.path.sep), lines_actual[i]
-            )
+            self.assertEqual(lines_expected[i].replace("/", os.path.sep), lines_actual[i])
 
         # Clean up the output file
         shutil.rmtree(os.path.dirname(output_file_path))
